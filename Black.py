aa655217cf439117af2b365df5809cf327664e48
MitsuruFujiwara <fujiwara52jp@gmail.com>
2016-04-30 15:13:27 +0900

    Add HW&BK

---
+++ Black.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy.stats as stats
+import math
+
+class Black:
+
+    def __init__(self, capVol, PDB, maturity, Rcap, L , tenor):
+        self.capVol = capVol
+        self.PDB = PDB
+        self.maturity = maturity
+        self.Rcap = Rcap
+        self.L = L
+        self.tenor = tenor
+        self.faceValue = L * (1 + Rcap * tenor)
+
+    def __N(self, x):
+        return stats.norm.cdf(x)
+
+    def __getFowardRates(self):
+        capV = self.capVol
+        P = self.PDB
+        t = self.maturity
+        tenor = self.tenor
+        R = []
+        F =[]
+
+        for i in range(0, len(capV) - 1):
+            r = -(1/t[i]) * (np.log(P[i]))
+            f = -(1/tenor) * np.log(P[i+1] / P[i])
+            R.append(r)
+            F.append(f)
+        self.R = R
+        self.F = F
+
+    def priceBlackCap(self):
+        self.__getFowardRates()
+
+        F = self.F
+        R = self.R
+        P = self.PDB
+        faceValue = self.faceValue
+        Rcap = self.Rcap
+        capV = self.capVol
+        t = self.maturity
+        tenor = self.tenor
+
+        caplet = []
+        for i in range(0, len(capV) - 1):
+            cap = self.__BlackFormula(F[i], P[i], faceValue, Rcap, capV[i], t[i], tenor)
+            caplet.append(cap)
+
+        return caplet
+
+    def __BlackFormula(self, F, P, L, Rcap, vol, tau, dtau):
+        # compute cap price with Black's 1976 model
+        d1 = np.log(F / Rcap) + (0.5 * vol * vol * tau )/ (vol * np.sqrt(tau))
+        d2 = d1 - vol * np.sqrt(tau)
+
+        return P * dtau * L * self.__N(d1) - Rcap * self.__N(d2)
+
+if __name__ == '__main__':
+    capVol = []
+    PDB = []
+    mturity = []
+    Rcap = 0.5
+
+    b = Black()
---
+++ HW_test.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy.stats as stats
+import math
+
+import Black
+
+class PriceCapHW:
+
+    def __init__(self, mats, vols, rates, a, FV, vol, Rcap, tenor, L):
+        self.mats = mats
+        self.vols = vols
+        self.rates = rates
+        self.a = a
+        self.FV = FV
+        self.vol = vol
+        self.Rcap = Rcap
+        self.tenor = tenor
+        self.L = L
+
+    def __P(self):
+        mats = self.mats
+        rates = self.rates
+
+        P = []
+        for i, t in enumerate(mats):
+            DF_t = math.exp(- t * rates[i])
+            P.append(DF_t)
+
+        self.P = P
+        return self.P
+
+    def __N(self, x):
+        return stats.norm.cdf(x)
+
+    def priceHWCap(self):
+        # pricing Cap value with Hull-White model
+        vols = self.vols
+        P = self.__P()
+        mats = self.mats
+        Rcap = self.Rcap
+        L = self.L
+        tenor = self.tenor
+
+        BK = Black.Black(vols, P, mats, Rcap, L, tenor)
+        self.market = BK.priceBlackCap()
+        model = []
+
+        for i in range(0, len(vols) - 1):
+            volP = np.sqrt(((vol * vol) / (2 * a * a * a))  * (1 - math.exp(-2 * mats[i] * a)) \
+            * (1 - math.exp(-a * (mats[i+1] - mats[i]))) ** 2)
+
+            d1 = math.log((FV * P[i + 1])/(L * P[i]))/ volP +  0.5 * volP
+            d2 = d1 - volP
+            tmp = L * P[i] * self.__N(-d2) - FV * P[i+1] * self.__N(-d1)
+
+            model.append(tmp)
+
+        return model
+
+    def calibrateToMarketPrice(self):
+        # get squared errors between market quote and model
+        model = self.priceHWCap()
+        market = self.market
+
+        se = 0.0
+        for i in range(0, len(vols) - 1):
+            se += ((model[i] - market[i]) / market[i]) ** 2
+
+        return se
+
+#    def getParameters(self):
+        # two dimensional Newton-Raphson minimization rutine
+
+
+if __name__ == '__main__':
+    mats = [1.0, 2.0 ,3.0, 4.0] # caplet maturities
+    vols = [0.5, 0.5, 0.5, 0.5] # market quote caplet volatilities
+    rates = [0.01, 0.02, 0.03, 0.04] # short rates
+    Rcap = 0.05 # cap rate
+    a = 0.001 # mean reversion parameter
+    FV = 100 # face value of loan
+    vol = 0.5 # length between payment time
+    tenor = 0.5 # length of time between payment date
+    L = 100 # principal amount of loan
+
+    HW = PriceCapHW(mats, vols, rates, a, FV, vol, Rcap, tenor, L)
+
+    print HW.calibrateToMarketPrice()
